4dd275a29d9a9e810a9a8e6744f2715ac42c1e98
Amélie Aimar <pelicarpa@hotmail.fr>
2025-01-21 12:42:39 +0100

    exercice_deconnexion

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,13 +5,28 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        $product = new User();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setNom('Sa Bento')->setPrenom('Tony');
+        $user->setEmail('pelicarpa@hotmail.fr');
+        $encoded = $this->encoder->hashPassword($user, '123');
+        $user->setPassword($encoded);
+        $user->setRoles(['ROLE_USER']);
+
+
+        $manager->persist($user);
 
         $manager->flush();
     }
