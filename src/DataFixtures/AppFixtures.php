f4031a7b46cd2dd2c0bf6dd850375561fdb0066d
Amélie Aimar <pelicarpa@hotmail.fr>
2025-01-21 12:56:35 +0100

    creation_utilisateurs

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -26,7 +26,25 @@ class AppFixtures extends Fixture
         $user->setRoles(['ROLE_USER']);
 
 
+        $admin = new User();
+        $admin->setNom('Houvenaeghel')->setPrenom('Margot');
+        $admin->setEmail('Houvenaeghel@hotmail.fr');
+        $encodedAdmin = $this->encoder->hashPassword($admin, '123');
+        $admin->setPassword($encodedAdmin);
+        $admin->setRoles(['ROLE_ADMIN']);
+
+
+        $employee = new User();
+        $employee->setNom('Aimar')->setPrenom('Amelie');
+        $employee->setEmail('Aimar@hotmail.fr');
+        $encodedEmployee = $this->encoder->hashPassword($employee, '123');
+        $employee->setPassword($encodedEmployee);
+        $employee->setRoles(['ROLE_EMPLOYEE']);
+
+
         $manager->persist($user);
+        $manager->persist($admin);
+        $manager->persist($employee);
 
         $manager->flush();
     }
