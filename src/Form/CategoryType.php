5b7eee3883fbc730143c37c34a89607c6c7ba948
Amélie Aimar <pelicarpa@hotmail.fr>
2025-01-26 07:47:53 +0100

    upload_img_1

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class CategoryType extends AbstractType
@@ -16,7 +18,29 @@ class CategoryType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-            ->add('image')
+            ->add(
+                'image',
+                FileType::class,
+                [
+                    'required' => false,
+                    'mapped' => false,
+                    'label' => "Image Catégorie",
+                    'attr' => [
+                        'placeholder' => 'Placeholder Image Catégorie'
+                    ],
+                    'constraint' => [
+                        new File([
+                            'maxSize' => '1024K',
+                            'mimeTypes' => [
+                                'image/jpeg',
+                                'image/png'
+                            ],
+                            'mimeTypeMessage' => 'Merci de charger un fichier jpg ou png',
+                            'uploadFormSizeErrorMessage' => 'La taille max autorisée est de 1024 kO',
+                        ])
+                    ],
+                ]
+            )
             ->add('date_add', DateTimeType::class, [
                 'data' => new \DateTime(),
                 'widget' => 'single_text'
