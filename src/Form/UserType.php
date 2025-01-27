7a1f7f0fd5cf4ead1be6e96e5950a8b444b09303
Amélie Aimar <pelicarpa@hotmail.fr>
2025-01-27 19:02:51 +0100

    mot_de_passe

---
+++ src/Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Client' => 'ROLE_USER',
+                    'Employe' => 'ROLE_EMPLOYEE',
+                    'Admin' => 'ROLE_ADMIN'
+                ],
+                'help' => 'Sélectionner un rôle',
+                'multiple' => false,
+                'mapped' => false,
+            ])
+            ->add('password', null, [
+                'data' => "",
+                'required' => false
+            ])
+            ->add('nom')
+            ->add('prenom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
